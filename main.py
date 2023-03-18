de10713ee18cdb95a5aa7fba0ecc779a0972f101
not a droid <68233166+Codeblocc@users.noreply.github.com>
2023-03-18 23:14:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 # refer https://pypi.org/project/webdriver-manager/
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 os.environ['PATH'] += r"C:/SeleniumDriver"
 options = webdriver.ChromeOptions()  # Added with research
@@ -17,4 +14,3 @@ driver.get('https://discord.com/api/downloads/distributions/app/installers/lates
 driver.maximize_window()
 my_element = driver.find_element("id", "downloadButton")
 my_element.click()
-
